fbf0f9357af3300905e15006556f2f5a6471a4cb
Bartłomiej Szlachta <bszlachta1024@gmail.com>
2022-03-16 20:53:15 +0100

    Load invoice data if invoice id is given

---
+++ js/form.js
@@ -0,0 +1,26 @@
+const initializeFormValues = () => {
+  const id = new URLSearchParams(window.location.search).get('id');
+  const headerElement = document.getElementById('header');
+  if (!id) {
+    headerElement.innerText = 'Nowa faktura';
+    return;
+  }
+
+  headerElement.innerText = 'Modyfikacja faktury';
+  const loadingAnimationElement = document.getElementById('loading-animation');
+  loadingAnimationElement.classList.remove('d-none');
+
+  getInvoice(id)
+    .then(data => {
+
+    })
+    .catch(error => {
+      const errorMessageElement = document.getElementById('error-message');
+      errorMessageElement.classList.remove('d-none');
+    })
+    .finally(() => {
+      loadingAnimationElement.classList.add('d-none');
+    });
+};
+
+initializeFormValues();
