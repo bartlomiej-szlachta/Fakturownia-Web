a1b0802d98bbc1de4edf04164b618c318fe83781
Bartłomiej Szlachta <bszlachta1024@gmail.com>
2022-03-19 21:34:01 +0100

    Add deleting invoices

--- js/list.js
+++ js/list.js
@@ -6,20 +6,47 @@ const navigateToFormEditMode = (id) => {
   window.location.href = `form.html?id=${id}`;
 };
 
+const requestDeleteInvoice = (id) => {
+  const loadingAnimationElement = document.getElementById('loading-animation');
+  const errorMessageElement = document.getElementById('error-message');
+
+  showElement(loadingAnimationElement);
+  hideElement(errorMessageElement);
+
+  deleteInvoice(id)
+    .then(result => {
+      document.getElementById(id).remove();
+    })
+    .catch(result => {
+      showElement(errorMessageElement);
+    })
+    .finally(() => {
+      hideElement(loadingAnimationElement);
+    });
+};
+
 const addInvoiceListItem = (invoiceData) => {
   const listElement = document.getElementById('invoices-list');
 
   const editInvoiceButtonId = `button-edit-invoice--${invoiceData.id}`;
+  const deleteInvoiceButtonId = `button-delete-invoice--${invoiceData.id}`;
   const liElement = document.createElement('li');
+  liElement.id = invoiceData.id;
   liElement.innerHTML = `
     <button id="${editInvoiceButtonId}">
       Edytuj
+    </button>
+    <button id="${deleteInvoiceButtonId}">
+      Usuń
     </button>
   `;
   listElement.appendChild(liElement);
 
   document.getElementById(editInvoiceButtonId)
     .addEventListener('click', () => navigateToFormEditMode(invoiceData.id));
+
+  document.getElementById(deleteInvoiceButtonId)
+    .addEventListener('click', () => requestDeleteInvoice(invoiceData.id));
 };
 
 const initializeInvoicesList = () => {
