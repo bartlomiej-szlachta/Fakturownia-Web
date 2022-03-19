393fa79cde15700c60a6eea8947907a037870f3a
Bartłomiej Szlachta <bszlachta1024@gmail.com>
2022-03-19 21:08:01 +0100

    Display invoices list

--- js/list.js
+++ js/list.js
@@ -2,4 +2,44 @@ const navigateToFormAddMode = () => {
   window.location.href = 'form.html';
 };
 
+const navigateToFormEditMode = (id) => {
+  window.location.href = `form.html?id=${id}`;
+};
+
+const addInvoiceListItem = (invoiceData) => {
+  const listElement = document.getElementById('invoices-list');
+
+  const editInvoiceButtonId = `button-edit-invoice--${invoiceData.id}`;
+  const liElement = document.createElement('li');
+  liElement.innerHTML = `
+    <button id="${editInvoiceButtonId}">
+      Edytuj
+    </button>
+  `;
+  listElement.appendChild(liElement);
+
+  document.getElementById(editInvoiceButtonId)
+    .addEventListener('click', () => navigateToFormEditMode(invoiceData.id));
+};
+
+const initializeInvoicesList = () => {
+  const loadingAnimationElement = document.getElementById('loading-animation');
+  const errorMessageElement = document.getElementById('error-message');
+
+  showElement(loadingAnimationElement);
+
+  getAllInvoices()
+    .then((data) => {
+      data.forEach(addInvoiceListItem);
+    })
+    .catch(error => {
+      showElement(errorMessageElement);
+    })
+    .finally(() => {
+      hideElement(loadingAnimationElement);
+    });
+};
+
+initializeInvoicesList();
+
 document.getElementById('button-add-invoice').addEventListener('click', navigateToFormAddMode);
