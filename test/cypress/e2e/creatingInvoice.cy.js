ff1a7fa66db9f523cd98669c7bcb57a1e6841371
Bartłomiej Szlachta <bszlachta1024@gmail.com>
2022-06-14 10:37:06 +0200

    Add creatingInvoice.cy.js spec

---
+++ test/cypress/e2e/creatingInvoice.cy.js
@@ -0,0 +1,19 @@
+describe('Dodawanie faktury', () => {
+
+  it('Nowo dodana faktura powinna się pojawić na liście', () => {
+    cy.visit('list.html');
+    cy.wait(1000);
+    cy.removeAllInvoices();
+    cy.get('#button-add-invoice').click();
+    cy.get('#seller_name').type('Super sprzedawca');
+    cy.get('#seller_tax_no').type('0000000000');
+    cy.get('#buyer_name').type('Super nabywca');
+    cy.get('#buyer_tax_no').type('0000000000');
+    cy.get('.input--name').type('Nazwa pozycji');
+    cy.get('.input--total_price_gross').clear().type('10.00');
+    cy.get('#button-save-invoice').click();
+    cy.contains('Sprzedawca: Super sprzedawca');
+    cy.contains('Nabywca: Super nabywca');
+  });
+
+});
