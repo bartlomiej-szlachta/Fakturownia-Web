1b65af017a61c114804b58ee9e45ab92ff77f7e4
Bartłomiej Szlachta <bszlachta1024@gmail.com>
2022-06-18 21:42:34 +0200

    Add test for input values

--- test/cypress/e2e/form.cy.js
+++ test/cypress/e2e/form.cy.js
@@ -11,6 +11,20 @@ describe('Formularz dodawania / edycji faktury', () => {
       cy.contains('Nowa faktura');
     });
 
+    it('Powinien zawierać pola wypełnione inicjalnymi danymi', () => {
+      const todayDate = new Date().toISOString().split('T')[0];
+      cy.get('#issue_date').should('have.value', todayDate);
+      cy.get('#sell_date').should('have.value', todayDate);
+      cy.get('#seller_name').should('have.value', '');
+      cy.get('#seller_tax_no').should('have.value', '');
+      cy.get('#buyer_name').should('have.value', '');
+      cy.get('#buyer_tax_no').should('have.value', '');
+      cy.get('.input--name').should('have.value', '');
+      cy.get('.input--tax').find('option:selected').should('have.value', '23%');
+      cy.get('.input--total_price_gross').should('have.value', '0.00');
+      cy.get('.input--quantity').should('have.value', '1');
+    });
+
   });
 
   describe('W trybie edycji', () => {
@@ -24,6 +38,19 @@ describe('Formularz dodawania / edycji faktury', () => {
 
     it('Powinien wyświetlać odpowiedni nagłówek', () => {
       cy.contains('Modyfikacja faktury');
+    });
+
+    it('Powinien zawierać pola uzupełnione danymi wybranej faktury', () => {
+      cy.get('#issue_date').should('have.value', '2022-06-01');
+      cy.get('#sell_date').should('have.value', '2022-05-01');
+      cy.get('#seller_name').should('have.value', 'Super sprzedawca');
+      cy.get('#seller_tax_no').should('have.value', '0000000000');
+      cy.get('#buyer_name').should('have.value', 'Super nabywca');
+      cy.get('#buyer_tax_no').should('have.value', '1111111111');
+      cy.get('.input--name').should('have.value', 'Nazwa pozycji');
+      cy.get('.input--tax').find('option:selected').should('have.value', '5%');
+      cy.get('.input--total_price_gross').should('have.value', '10.00');
+      cy.get('.input--quantity').should('have.value', '2');
     });
 
   });
